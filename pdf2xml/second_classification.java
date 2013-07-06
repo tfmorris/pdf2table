3efac9f329ddd29d870add0fabfa6eeca8fc85cf
Tom Morris <tfmorris@gmail.com>
2013-07-05 23:52:59 -0400

    Fix a few stray merge conflicts (in comments)

--- pdf2xml/second_classification.java
+++ pdf2xml/second_classification.java
@@ -2,24 +2,7 @@
     Copyright 2005, 2005 Burcu Yildiz
     Contact: burcu.yildiz@gmail.com
     
-<<<<<<< HEAD
     This file is part of pdf2table.    pdf2table is free software: you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation, either version 3 of the License, or    (at your option) any later version.    pdf2table is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.    You should have received a copy of the GNU General Public License    along with pdf2table.  If not, see <http://www.gnu.org/licenses/>.
-=======
-    This file is part of pdf2table.
-
-    pdf2table is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    pdf2table is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with pdf2table.  If not, see <http://www.gnu.org/licenses/>.
->>>>>>> mac_original
 */
 
 package pdf2xml;
